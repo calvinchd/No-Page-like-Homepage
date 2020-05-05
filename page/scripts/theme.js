5d32d3184dbb2da4f66493a03c2498b79c0757b5
Calvin Chu <calvinchd@gmail.com>
2020-05-05 18:25:18 +1000

    feat: dark/light theme toggle

---
+++ page/scripts/theme.js
@@ -0,0 +1,35 @@
+const themeDir="styles/themes/";
+const themeLight="gruvbox-light";
+const themeDark="gruvbox-dark";
+
+window.addEventListener("load", initTheme, true);
+
+// Load saved theme
+function initTheme() {
+	if (localStorage.getItem("theme") === "light") {
+		document.getElementById('theme').href = setLight() + ".css";
+	}
+}
+
+function toggleTheme() {
+	var link = document.getElementById('theme');
+	var current = link.href.split('/').pop();
+	if (current === (themeDark + ".css")){
+		current = setLight();
+	} else {
+		current = setDark();
+	}
+	link.href = current + ".css";
+}
+
+// Set theme and save in local storage
+function setDark() {
+	document.getElementById('themeButton').innerHTML = "&#x1F31E;";
+	localStorage.removeItem("theme");
+	return current = themeDir + themeDark;
+}
+function setLight() {
+	document.getElementById('themeButton').innerHTML = "&#x1F311;";
+	localStorage.setItem("theme", "light");
+	return themeDir + themeLight;
+}
